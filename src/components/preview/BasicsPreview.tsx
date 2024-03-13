fb8544e6b1c371f4094a8da57876249545fe982a
Daniel Arteta <danielatts97@hotmail.com>
2024-03-13 13:46:40 -0500

    feat: add image preview

--- src/components/preview/BasicsPreview.tsx
+++ src/components/preview/BasicsPreview.tsx
@@ -16,6 +16,14 @@ export default function BasicsPreview({ resume, setResume }: Props) {
       const { name, value } = e.target as HTMLInputElement;
       setResume({ ...resume(), basics: { ...basics, [name]: value } });
     });
+
+    let $image = document.querySelector('#image');
+    let $imagePreview = document.querySelector<HTMLMediaElement>('#imagePreview');
+    $image.addEventListener('change', (e) => {
+      $imagePreview.classList.remove('hidden');
+      $imagePreview.src = URL.createObjectURL((e.target as HTMLInputElement).files[0]);
+      console.log($imagePreview.src)
+    })
   });
 
   return (
@@ -23,6 +31,7 @@ export default function BasicsPreview({ resume, setResume }: Props) {
       <h1 class="font-bold text-2xl text-left">{resume().basics.name}</h1>
       <h2 class="font-medium text-base text-left text-neutral-500">{resume().basics.label}</h2>
       <p class="text-sm text-neutral-500 text-pretty">{resume().basics.summary}</p>
+      <image class="hidden" id="imagePreview" width={50} height={50} />
     </div>
   );
 }
