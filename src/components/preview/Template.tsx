194a7aecc6e3d4bcd2c8cd6e0f5baceadff19bd8
Daniel Arteta <danielatts97@hotmail.com>
2024-03-18 10:58:22 -0500

    feat: add resume schema

--- src/components/preview/Template.tsx
+++ src/components/preview/Template.tsx
@@ -1,14 +1,14 @@
 import type { Resume } from '@/types';
 import BasicsPreview from '@/components/preview/BasicsPreview';
-import placeholder from '@/resources/placeholder';
+import resumeSchema from '@/resources/resumeSchema';
 import { createStore } from 'solid-js/store';
 
 export default function Template() {
-    const [store, setStore] = createStore<Resume>(placeholder);
+    const [store, setStore] = createStore<Resume>(resumeSchema);
 
     return (
         <>
-            <BasicsPreview resume={store} setResume={setStore} />
+            <BasicsPreview basics={store.basics} setBasics={setStore} />
         </>
     );
 }
